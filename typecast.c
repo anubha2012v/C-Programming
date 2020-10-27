e9eb4e425ca0e814a83d7de379d3709f0faed1c5
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2020-10-27 11:23:08 +0530

    Update typecast.c

--- typecast.c
+++ typecast.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int a=3;
-    float b= (float)54/5; //Typecasting
+    int a = 3;
+    float b = (float)54/5; //Typecasting
     printf("The value of a is %f\n", b);
     return 0;
 }
