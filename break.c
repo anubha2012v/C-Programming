2688d6b79a7172ec29d91c4dc626033a807f462d
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2020-10-27 11:17:41 +0530

    Update break.c

--- break.c
+++ break.c
@@ -3,7 +3,7 @@ int main(int argc, char const *argv[])
 {
     printf("Hello\n");
     int i, age;
-    for(i=0; i<10; i++){
+    for(i= 0; i<10; i++){
         printf("%d\n Enter your age\n", i);
         scanf("%d", &age);
         if (age>10){
