efe0d1ce286d6f5ebd2386c06f96f52aa99031d7
anubha2012v <singh.anubhav2012@gmail.com>
2020-10-27 11:15:56 +0530

    Program

---
+++ mul.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter the Number: ");
+    scanf("%d",&a);
+    printf("\n  %d x 1: %d",a, a * 1);
+    printf("\n  %d x 2: %d",a ,a * 2);
+    printf("\n  %d x 3: %d",a, a * 3);
+    printf("\n  %d x 4: %d",a, a * 4);
+    printf("\n  %d x 5: %d",a, a * 5);
+    printf("\n  %d x 6: %d",a, a * 6);
+    printf("\n  %d x 7: %d",a, a * 7);
+    printf("\n  %d x 8: %d",a, a * 8);
+    printf("\n  %d x 9: %d",a, a * 9);
+    printf("\n  %d x 10: %d",a, a * 10);
+    return 0;
+}
