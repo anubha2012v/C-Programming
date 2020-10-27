58c10903bc250413e05366227ff768ae054ac685
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2020-10-27 11:19:20 +0530

    Update continue.c

--- continue.c
+++ continue.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
     int i, age;
     for (i = 0; i < 10; i++)
     {
-        printf("%d Enter Your age\n", i);
+        printf("%d Enter Your age: \n", i);
         scanf("%d", &age);
 
         if (age > 10)
