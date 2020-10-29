73ed450ef14d2affb279213a02d1b90acc10f719
anubha2012v <singh.anubhav2012@gmail.com>
2020-10-29 18:21:54 +0530

    Call By reference example.

---
+++ callbyreference.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    return;
+}
+
+int main()
+{
+    int a = 34, b = 74;
+    printf("%d and %d\n", a, b);
+    swap(&a, &b); // funtion called using call by reference
+    printf("%d and %d\n", a, b);
+    return 0;
+}
