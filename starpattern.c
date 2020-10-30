fde8ba6927e50a2ef02fc5b29ede71119ee91463
anubha2012v <singh.anubhav2012@gmail.com>
2020-10-30 10:19:31 +0530

    Star pattern and reverse star pattern using function.

---
+++ starpattern.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int i,j, a, b;
+    printf("Press 0 to Print Triangular star pattern and 1 for reverse star pattern:\n");
+    scanf("%d",&a);
+
+    printf("Enter the number of rows of the pattern:");
+    scanf("%d",&b);
+    
+
+    if (a == 0)
+    {
+        for ( i = 1; i < b; i++)
+        {
+            for ( j = 0; j < i; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        
+    }
+    else
+    {
+        for ( i = 1; i <= b; i++)
+        {
+            b--;
+            for ( j = 1; j < b + 1; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        
+    }
+    
+    return 0;
+}
